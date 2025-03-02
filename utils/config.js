f444fcb0d98b7b51a5b93d56b66b7477a5574543
Brandon <kaiutoshan@gmail.com>
2025-03-01 18:09:07 -0800

    Alpha 0.0.3 ServerUpdate

--- utils/config.js
+++ utils/config.js
@@ -1,9 +1,6 @@
-require("dotenv").config();
-
-const JWT_SECRET =
-  process.env.NODE_ENV === "production"
-    ? process.env.JWT_SECRET
-    : process.env.JWT_SECRET || "123456789123456789";
+const {
+  JWT_SECRET = "ad8787a9e6cebf374d1cf2acf10ffa7a8d1d7645c880a974986caeae3c13d9f4",
+} = process.env;
 
 module.exports = {
   JWT_SECRET,
