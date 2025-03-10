2ced065677a18882ad229636a7a0ec71c71092e3
Brandon <kaiutoshan@gmail.com>
2025-03-10 13:56:41 -0700

    Beta 0.1.2

--- app.js
+++ app.js
@@ -39,8 +39,10 @@ const corsOptions = {
   credentials: true,
 };
 
-app.use(cors(corsOptions));
+const corsPolicy =
+  process.env.NODE_ENV === "production" ? cors(corsOptions) : cors();
 
+app.use(corsPolicy);
 app.use(rateLimiter);
 
 app.use(requestLogger);
